b7f5bd56aa363e6a9cf401e49940cd9c5d22f632
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-10 15:52:30 -0700

    Added while loop to remove whitespace from the front of the input string and comment explaining what while loop does

--- lab1/Scanner.h
+++ lab1/Scanner.h
@@ -2,6 +2,7 @@
 #include "Token.h"
 #include <string>
 #include <sstream>
+#include <cctype>
 
 using namespace std;
 
@@ -18,5 +19,14 @@ public:
         string value = ",";
         int line = 4;
         return Token(type, value, line);
+
+        /*
+        while the first character in the input is whitespace,
+        create substring starting with the second character
+        */
+        while (isspace(input.at(0)))
+        {
+            input = input.substr(1);
+        }
     }
 };
