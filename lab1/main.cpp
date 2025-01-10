afd81b33f37a96f9ed93f7d42c3fcf9cdf1afe64
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-10 15:38:22 -0700

    included Scanner.h and implemented scanToken function in main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 #include "Token.h"
+#include "Scanner.h"
 
 using namespace std;
 
 int main()
 {
-    Token t = Token(COMMA, ",", 2);
+    Scanner s = Scanner(" , , ");
+    Token t = s.scanToken();
     cout << t.toString() << endl;
 
     return 0;
