059e18125bcb7c3c61a4915c2834be7ebff31877
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-31 10:34:07 -0700

    Added support functions listed in lab instructions

--- lab2/Parser.h
+++ lab2/Parser.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Token.h"
+#include <iostream>
 #include <vector>
+
+using namespace std;
 
 class Parser {
     private:
@@ -8,4 +11,16 @@ class Parser {
     
     public:
     Parser (const vector<Token>& tokens) : tokens(tokens) {}
-}
+    
+    TokenType tokenType() const{
+        return tokens.at(0).getType();
+    }
+    
+    void advanceToken() {
+        tokens.erase(tokens.begin());
+    }
+    
+    void throwError() {
+        cout << "error" << endl;
+    }
+};
