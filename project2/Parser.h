e2878946bdb8999ec8ccd7da871ab44e60adb8a3
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-31 15:14:24 -0700

    Copied over Parser.h and Token.h from lab2

---
+++ project2/Parser.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "Token.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Parser {
+    private:
+    vector<Token> tokens;
+    
+    public:
+    Parser (const vector<Token>& tokens) : tokens(tokens) {}
+    
+    TokenType tokenType() const{
+        return tokens.at(0).getType();
+    }
+    
+    void advanceToken() {
+        tokens.erase(tokens.begin());
+    }
+    
+    void throwError() {
+        cout << "error" << endl;
+    }
+    
+    /*
+    checks to see whether TokenType entered matches current token
+    prints out what token it is trying to match with, then advances token if it matches
+    error is thrown if the type doesn't match, and advanceToken is not called
+    */
+    void match(TokenType t) {
+        cout << "match: " << t << endl;
+        if (tokenType() == t) {
+            advanceToken();
+        }
+        else {
+            throwError();
+        }
+    }
+    
+    void idList () {
+        if(tokenType() == COMMA) {
+            match(COMMA);
+            match(ID);
+            idList();
+        }
+        else {
+            //lambda
+        }
+    }
+    
+    void scheme() {
+        if (tokenType() == ID) {
+            match(ID);
+            match(LEFT_PAREN);
+            match(ID);
+            idList();
+            match(RIGHT_PAREN);
+        }
+        else {
+            //lambda
+        }
+    }
+};
