206cfbb2e59fd24cbbf7dbb8341d711e18d3c5d2
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-31 10:55:24 -0700

    Added test code that creates a vector of 3 tokens, then uses Parser to print each token, advance the token, then call throwError()

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,6 +4,18 @@
 
 int main (int argc, char* argv[]) {
     
-    
+    vector<Token> tokens = {
+    Token(ID,"Ned",2),
+    Token(LEFT_PAREN,"(",2),
+    Token(RIGHT_PAREN,")",2),
+    };
+
+    Parser p = Parser(tokens);
+    cout << p.tokenType() << endl;
+    p.advanceToken();
+    cout << p.tokenType() << endl;
+    p.advanceToken();
+    cout << p.tokenType() << endl;
+    p.throwError();
     return 0;
 }
