2986e1275d2676efbcc77435b03170d8e4296362
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-31 10:29:48 -0700

    Added private vector variable to hold vector of tokens and constructor that takes a vector of tokens

--- lab2/Parser.h
+++ lab2/Parser.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Token.h"
+#include <vector>
 
 class Parser {
+    private:
+    vector<Token> tokens;
     
+    public:
+    Parser (const vector<Token>& tokens) : tokens(tokens) {}
 }
