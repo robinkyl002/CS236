e6802e809a86375f9c248aeea98b0cf1f8e3b1eb
Kyle Robinson <ky.robinson72@gmail.com>
2025-01-31 14:35:47 -0700

    Commented out previous test code and added new code to test valid idList() input

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,11 +4,11 @@
 
 int main (int argc, char* argv[]) {
     
-    vector<Token> tokens = {
-    Token(ID,"Ned",2),
-    Token(LEFT_PAREN,"(",2),
-    Token(RIGHT_PAREN,")",2),
-    };
+    // vector<Token> tokens = {
+    // Token(ID,"Ned",2),
+    // Token(LEFT_PAREN,"(",2),
+    // Token(RIGHT_PAREN,")",2),
+    // };
 
     // Parser p = Parser(tokens);
     // cout << p.tokenType() << endl;
@@ -18,10 +18,21 @@ int main (int argc, char* argv[]) {
     // cout << p.tokenType() << endl;
     // p.throwError();
     
-    Parser p = Parser(tokens);
-    p.match(ID);
-    p.match(LEFT_PAREN);
-    p.match(ID);         // intentional error
-    p.match(RIGHT_PAREN);
+    // Parser p = Parser(tokens);
+    // p.match(ID);
+    // p.match(LEFT_PAREN);
+    // p.match(ID);         // intentional error
+    // p.match(RIGHT_PAREN);
+    
+    vector<Token> tokens = {
+    Token(COMMA,",",2),
+    Token(ID,"Ted",2),
+    Token(COMMA,",",2),
+    Token(ID,"Zed",2),
+    Token(RIGHT_PAREN,")",2),
+  };
+
+  Parser p = Parser(tokens);
+  p.idList();
     return 0;
 }
