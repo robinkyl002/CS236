243aa355563f0afb33934298a9d125004f550821
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-06 09:25:51 -0700

    Added constructor and functions for predicates, adding predicate to a list, and matching a Tokentype

--- project2/Predicate.h
+++ project2/Predicate.h
@@ -1,10 +1,41 @@
+#pragma once
+#include "Parser.h"
+#include "Parameter.h"
 #include <string>
 #include <vector>
-#include "Parameter.h"
 
 class Predicate {
     private:
     string name;
     vector<Parameter> parameters;
     
+    public:
+    
+    void predicate() {
+    
+    
+    }
+    
+    void predicateList() {
+        if (TokenType() == COMMA) {
+            match(COMMA);
+            predicate();
+            predicateList();
+        }
+        else
+        {
+            //lambda
+        }
+        
+    }
+    
+    void match(TokenType t) {
+        cout << "match: " << t << endl;
+        if (tokenType() == t) {
+            //TODO: add code to make it add Token to correct list
+            advanceToken();
+        }
+        else {
+            throwError();
+        }
 }
