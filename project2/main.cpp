ac5f26e0c80c159ec55aa58517cf4c7e6d4c221d
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-10 13:00:34 -0700

    Included Parser.h file, and added call to parser function within the class

--- project2/main.cpp
+++ project2/main.cpp
@@ -4,6 +4,7 @@
 
 #include "Token.h"
 #include "Scanner.h"
+#include "Parser.h"
 
 using namespace std;
 
@@ -18,34 +19,39 @@ int main(int argc, char *argv[])
     string input = ss.str();
     in.close();
 
-//    cout << input << endl;
+    //    cout << input << endl;
 
     Scanner s = Scanner(input);
 
-//    Scanner s = Scanner("\n"
-//                        "\n"
-//                        "# unterminated string\n"
-//                        "\n"
-//                        "Schemes:\n"
-//                        "student(Name, ID, Address, Major)\n"
-//                        "\n"
-//                        "Facts:\n"
-//                        "student(North, '51, Main, CS).\n"
-//                        "\n"
-//                        "Rules:\n"
-//                        "major(Name,Major):-student(Name,ID,Address,Major).\n"
-//                        "\n"
-//                        "Queries:\n"
-//                        "major(North,Major)?\n"
-//                        "major(Name,MATH)?\n"
-//                        "\n"
-//                        "");
+    //    Scanner s = Scanner("\n"
+    //                        "\n"
+    //                        "# unterminated string\n"
+    //                        "\n"
+    //                        "Schemes:\n"
+    //                        "student(Name, ID, Address, Major)\n"
+    //                        "\n"
+    //                        "Facts:\n"
+    //                        "student(North, '51, Main, CS).\n"
+    //                        "\n"
+    //                        "Rules:\n"
+    //                        "major(Name,Major):-student(Name,ID,Address,Major).\n"
+    //                        "\n"
+    //                        "Queries:\n"
+    //                        "major(North,Major)?\n"
+    //                        "major(Name,MATH)?\n"
+    //                        "\n"
+    //                        "");
 
     tokens = s.scanAllTokens();
 
     // s.stringifyTokens();
-    
+
+    // cout << endl;
+    // cout << endl;
+
     Parser p = Parser(tokens);
+
+    p.parser();
 
     return 0;
 }
