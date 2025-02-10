269cb6edf8439e205c77d0ddfa7f2141298c4d9c
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-10 13:01:55 -0700

    set the constructor to take in a headPredicate and a vector of predicates. Also added a toString method

--- project2/Rule.h
+++ project2/Rule.h
@@ -1,14 +1,25 @@
 #pragma once
 #include "Parser.h"
 #include "Token.h"
+#include "Predicate.h"
 
-class Rule {
-    private: 
+class Rule
+{
+private:
     Predicate headPredicate;
     vector<Predicate> predicateList;
-    
-    public:
-    Rule() {}
-    
-    
-}
+
+public:
+    Rule(Predicate headPredicate, vector<Predicate> predicateList) : headPredicate(headPredicate), predicateList(predicateList) {}
+
+    string toString()
+    {
+        headPredicate.toString();
+        cout << ":-";
+        for (int i = 0; i < predicateList.size(); i++)
+        {
+            predicateList.at(i).toString();
+        }
+        cout << endl;
+    }
+};
