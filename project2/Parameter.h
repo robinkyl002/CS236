2c339f49c117fdf23838fc79e31010331724f0ef
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-10 13:12:01 -0700

    Created constructor and toString function to return the value of the Token passed

--- project2/Parameter.h
+++ project2/Parameter.h
@@ -1,6 +1,19 @@
+#pragma once
+#include "Token.h"
+#include <string>
 
+using namespace std;
 
-class Parameter {
-    private: 
-    
-}
+class Parameter
+{
+private:
+    Token token;
+
+public:
+    Parameter(Token token) : token(token) {}
+
+    string toString()
+    {
+        return token.getValue();
+    }
+};
