02bb1c6bb39dcaeb716b7a43284952b5d2825be0
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-10 13:05:09 -0700

    Added getter functions for each vector and add functions to allow the Parser to add new items to each vector

--- project2/DatalogProgram.h
+++ project2/DatalogProgram.h
@@ -7,14 +7,55 @@
 
 using namespace std;
 
-class DatalogProgram {
-    private: 
+class DatalogProgram
+{
+private:
     vector<Token> tokens;
     vector<Predicate> schemes;
     vector<Predicate> facts;
     vector<Rule> rules;
     vector<Predicate> queries;
-    
-    public:
-    void datalogProgram () {}
-}
+
+public:
+    void datalogProgram() {}
+
+    vector<Predicate> getSchemes()
+    {
+        return schemes;
+    }
+
+    vector<Predicate> getFacts()
+    {
+        return facts;
+    }
+
+    vector<Rule> getRules()
+    {
+        return rules;
+    }
+
+    vector<Predicate> getQueries()
+    {
+        return queries;
+    }
+
+    void addScheme(Predicate newScheme)
+    {
+        schemes.push_back(newScheme);
+    }
+
+    void addFact(Predicate newFact)
+    {
+        facts.push_back(newFact);
+    }
+
+    void addRule(Rule newRule)
+    {
+        rules.push_back(newRule);
+    }
+
+    void addQuery(Predicate newQuery)
+    {
+        queries.push_back(newQuery);
+    }
+};
