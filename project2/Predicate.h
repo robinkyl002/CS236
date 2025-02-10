5c89a2c61e9345dee6a7e6e11d656a4c3302f02c
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-10 13:15:24 -0700

    Added constructor to create a predicate from a name and a vector of parameters

--- project2/Predicate.h
+++ project2/Predicate.h
@@ -1,41 +1,79 @@
 #pragma once
 #include "Parser.h"
 #include "Parameter.h"
+#include <iostream>
 #include <string>
 #include <vector>
 
-class Predicate {
-    private:
+using namespace std;
+
+class Predicate
+{
+private:
     string name;
     vector<Parameter> parameters;
-    
-    public:
-    
-    void predicate() {
-    
-    
-    }
-    
-    void predicateList() {
-        if (TokenType() == COMMA) {
-            match(COMMA);
-            predicate();
-            predicateList();
-        }
-        else
+
+public:
+    Predicate(string name, vector<Parameter> parameters) : name(name), parameters(parameters) {}
+
+    // void predicate()
+    // {
+    // }
+
+    // void predicateList()
+    // {
+    //     if (TokenType() == COMMA)
+    //     {
+    //         // match(COMMA);
+    //         predicate();
+    //         predicateList();
+    //     }
+    //     else
+    //     {
+    //         // lambda
+    //     }
+    // }
+
+    // void match(TokenType t) {
+    //     cout << "match: " << t << endl;
+    //     if (tokenType() == t) {
+    //         //TODO: add code to make it add Token to correct list
+    //         advanceToken();
+    //     }
+    //     else {
+    //         throwError();
+    //     }
+    // }
+
+    string toString()
+    {
+        string predicate;
+
+        predicate = name + "(";
+
+        cout << name << "(";
+        for (int i = 0; i < parameters.size(); i++)
         {
-            //lambda
+            predicate += parameters.at(i).toString();
+            cout << parameters.at(i).toString();
+
+            if (i < parameters.size() - 1)
+            {
+                predicate += ", ";
+
+                cout << ", ";
+            }
+            else
+            {
+                predicate += ")";
+                predicate += ".";
+
+                cout << ")" << ".";
+            }
         }
-        
+
+        cout << predicate << endl;
+
+        return predicate;
     }
-    
-    void match(TokenType t) {
-        cout << "match: " << t << endl;
-        if (tokenType() == t) {
-            //TODO: add code to make it add Token to correct list
-            advanceToken();
-        }
-        else {
-            throwError();
-        }
-}
+};
