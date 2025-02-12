02f6eecd2f7a0bebafc1ea6e80f17716fd47d850
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-12 10:27:39 -0700

    Created Relation.h file and added code from lab instructions

---
+++ lab3/Relation.h
@@ -0,0 +1,29 @@
+//
+// Created by Kyle Robinson on 2/12/25.
+//
+
+#pragma once
+#include "Tuple.h"
+#include "Scheme.h"
+
+#include <string>
+#include <set>
+
+class Relation {
+
+private:
+
+    string name;
+    Scheme scheme;
+    set<Tuple> tuples;
+
+public:
+
+    Relation(const string& name, const Scheme& scheme)
+            : name(name), scheme(scheme) { }
+
+    void addTuple(const Tuple& tuple) {
+        tuples.insert(tuple);
+    }
+
+};
