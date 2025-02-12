a5bced51936b9771c53e1321dbd9896659034122
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-12 10:08:37 -0700

    Added main code from lab instructions to test Tuple and Scheme

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Kyle Robinson on 2/12/25.
 //
+
+int main() {
+
+    vector<string> names = { "ID", "Name", "Major" };
+
+    Scheme scheme(names);
+
+    vector<string> values[] = {
+            {"'42'", "'Ann'", "'CS'"},
+            {"'32'", "'Bob'", "'CS'"},
+            {"'64'", "'Ned'", "'EE'"},
+            {"'16'", "'Jim'", "'EE'"},
+    };
+
+    for (auto& value : values) {
+        Tuple tuple(value);
+        cout << tuple.toString(scheme) << endl;
+    }
+
+}
